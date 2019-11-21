a134bfd3e36294fce279dd8b18a1bcdeb091b9c1
mediatrice8 <kampiremediatrice@gmail.com>
2019-11-21 17:54:26 +0200

    updates

--- carapp/urls.py
+++ carapp/urls.py
@@ -13,11 +13,12 @@ urlpatterns = [
     url(r'logout/',views.logout_view,name='logout'),
     url(r'^carDetails/(\d+)$',views.carDetails,name='carDetails'),
     url(r'^cart/$',views.cart,name='cart'),
-    url(r'', views.default_map, name="default"),
     url(r'^addToCart/(\d+)/$', views.addToCart, name="addToCart"),
     url(r'profile/(\d+)', views.profile_view, name = 'profile'),
     url(r'update_profile/', views.update_profile, name = 'update_profile'),
     url(r'^api/merch/$', views.MerchList.as_view()),
+    
+     url(r'', views.default_map, name="default"),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
